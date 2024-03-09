ea8328f800eed648f85f2689484bbb6783f9c85b
python533 <143389231+python533@users.noreply.github.com>
2024-03-09 11:25:49 +0300

    Switch Case
    Java

---
+++ deneme.java
@@ -0,0 +1,30 @@
+import java.util.Scanner
+
+public class Main{
+
+public Static Void Main{
+System.out.Println("Gün Sayısını Giriniz:");
+int gun=0;
+Scanner sc=new Scanner(System.in);
+gun=sc.nextınt();
+
+switch (gun) {
+case 1:
+System.out.Println("Pazartesi");
+case 2:
+System.out.Println("Salı");
+case 3:
+System.out.Println("Çarşamba");
+case 4:
+System.out.Println("Perşembe");
+case 5:
+System.out.Println("Cuma");
+case 6:
+System.out.Println("Cumartesi");
+case 7:
+System.out.Println("Pazar");
+break;
+}
+
+}
+}
